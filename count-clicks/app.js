b684a8096ef96ab894f2f5080e777bc983d040b5
UP210342 <up210342@alumnos.upa.edu.mx>
2024-03-04 22:12:07 -0600

    3/4/24

---
+++ count-clicks/app.js
@@ -0,0 +1,22 @@
+let x = 1
+let y = 4
+let c = y - x
+
+let name = "aaaa"
+let name2 = 'eeeee'
+let name3 = `iiii`; //se usa para escribir con enters en el texto ` `
+
+let a = true && false || false;
+let b = !false;
+
+let min_number = 1 < 0;
+
+alert(c)
+
+//COMENTARIOS
+
+/* COMENTARIO */
+
+let button = document.getElementById("btn");
+window.navigator;
+console.log(button);
